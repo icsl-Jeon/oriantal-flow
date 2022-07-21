c6262265b2fc2d5773116279677205e15cd1ac8a
B.S Felipe Jeon <junbs95@gmail.com>
2022-07-21 21:56:34 +0900

    Complete landing page

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,70 +1,95 @@
 import LandingHeader from "../components/LandingHeader";
 
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import {Button, Row} from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import { Button, Row, Col, Container } from "react-bootstrap";
 
 function LandingPage() {
-    return (
-        <div>
-            <LandingHeader/>
-            <main className="p-3">
-                <Row style={{height: "300px"}}>
-                    <div className=" d-flex flex-column justify-content-center " style={{height: "327px"}}>
-                        <div className="display-3 mb-3"> Mission block</div>
-                        <div className="display-5 mb-5"> Supporting message ...</div>
-                        <div>
-                                <Button variant="success"> Sign up </Button>
-                        </div>
-                    </div>
+  return (
+    <div>
+      <LandingHeader />
+      <Container className="p-1">
+        <Row style={{ height: "300px" }}>
+          <div
+            className=" d-flex flex-column justify-content-center "
+            style={{ height: "327px" }}
+          >
+            <div className="display-3 mb-3"> Mission block</div>
+            <div className="display-5 mb-5"> Supporting message ...</div>
+            <div>
+              <Button variant="success"> Sign up </Button>
+            </div>
+          </div>
+        </Row>
 
-                </Row>
+        <Row style={{}}>
+          <CardGroup className="pe-5 ps-5">
+            <Card className="card border-light">
+              <Card.Body>
+                <Card.Title>Touch the numbers</Card.Title>
+                <Card.Text>Explore the communities</Card.Text>
+              </Card.Body>
+              <div className="pb-3">
+                <Button variant="outline-success"> Explore </Button>
+              </div>
+            </Card>
 
-                <Row style={{height: "300px"}}>
-                    <CardGroup className="pe-5 ps-5">
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Touch the numbers</Card.Title>
-                                <Card.Text>
-                                    Explore the communities
-                                </Card.Text>
-                            </Card.Body>
-                            <div className="pb-3">
-                                <Button variant="outline-success"> Explore </Button>
-                            </div>
-                        </Card>
+            <Card className="card border-light">
+              <Card.Body>
+                <Card.Title>Create your own quant strategy</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to
+                  additional content.{" "}
+                </Card.Text>
+              </Card.Body>
+              <div className="pb-3">
+                <Button variant="outline-success"> Make strategy </Button>
+              </div>
+            </Card>
 
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Create your own quant strategy</Card.Title>
-                                <Card.Text>
-                                    This card has supporting text below as a natural lead-in to
-                                    additional content.{' '}
-                                </Card.Text>
-                            </Card.Body>
-                            <divc className="pb-3">
-                                <Button variant="outline-success"> Make strategy </Button>
-                            </divc>
-                        </Card>
+            <Card className="card border-light">
+              <Card.Body>
+                <Card.Title>Extract insight from factful numbers</Card.Title>
+                <Card.Text>
+                  Analyze correlation between data and event.
+                </Card.Text>
+              </Card.Body>
+              <div className="pb-3">
+                <Button variant="outline-success"> Analyze </Button>
+              </div>
+            </Card>
+          </CardGroup>
+        </Row>
 
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Extract insight from factful numbers</Card.Title>
-                                <Card.Text>
-                                    Analyze correlation between data and event.
-                                </Card.Text>
-                            </Card.Body>
-                            <div className="pb-3">
-                                <Button variant="outline-success"> Analyze </Button>
-                            </div>
-                        </Card>
-                    </CardGroup>
-                </Row>
+        <Row className="p-2 border mt-5">
+          <Col className="border col-4 ">
+            {" "}
+            <Row className="display-5"> Explore </Row>{" "}
+            <Row> Read the insights </Row>
+          </Col>
+          <Col className="border">Describing picture </Col>
+        </Row>
 
-            </main>
-        </div>
+        <Row className="p-2 border">
+          <Col className="border col-4 ">
+            {" "}
+            <Row className="display-5"> Explore </Row>{" "}
+            <Row> Read the insights </Row>
+          </Col>
+          <Col className="border">Describing picture </Col>
+        </Row>
 
-    );
+        <Row className="p-2 border">
+          <Col className="border col-4 ">
+            {" "}
+            <Row className="display-5"> Explore </Row>{" "}
+            <Row> Read the insights </Row>
+          </Col>
+          <Col className="border">Describing picture </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default LandingPage;
